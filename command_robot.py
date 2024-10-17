908b50c40d74020cc317e1b6bc97afbad5f6190d
Larry O'Brien <lobrien@knowing.net>
2024-10-17 13:50:01 -1000

    Composed subcommands in auto

--- command_robot.py
+++ command_robot.py
@@ -10,6 +10,7 @@ import ntcore
 os.environ["HALSIMXRP_HOST"] = "192.168.42.1"
 os.environ["HALSIMXRP_PORT"] = "3540"
 
+# Never changes
 class CommandRobot(wpilib.TimedRobot):
     def robotInit(self):
         self.container = MyContainer()
@@ -31,6 +32,7 @@ class CommandRobot(wpilib.TimedRobot):
     def testInit(self):
         commands2.CommandScheduler.getInstance().cancelAll()
 
+# Never changes (?).
 class MyContainer:
     def __init__(self):
         self.drive = DriveSubsystem()
@@ -38,6 +40,7 @@ class MyContainer:
     def getAutonomousCommand(self):
         return AutonomousCommand(self.drive)
 
+# Definitely changes w hardware, but not so much as commands
 class DriveSubsystem(commands2.SubsystemBase):
     def __init__(self):
         self.io = xrp.XRPOnBoardIO()
@@ -56,7 +59,10 @@ class DriveSubsystem(commands2.SubsystemBase):
     def drive(self, speed: float, rotation: float):
         self.driver.arcadeDrive(speed, rotation)
         
-class AutonomousCommand(commands2.CommandBase):
+def AutonomousCommand(drive: DriveSubsystem):
+       return CircleCWForward(drive).andThen(CircleCWBack(drive))
+
+class CircleCWForward(commands2.CommandBase):
     def __init__(self, drive: DriveSubsystem):
         self.drive_system = drive
         self.addRequirements(drive)
@@ -70,6 +76,29 @@ class AutonomousCommand(commands2.CommandBase):
 
     def execute(self):
         self.drive_system.drive(1.0, -0.5)
+
+    def isFinished(self):
+        return self.timer.get() > 2.0
+    
+    def end(self, interrupted : bool):
+        self.drive_system.io.setLed(False)
+        self.drive_system.drive(0, 0)
+        
+
+class CircleCWBack(commands2.CommandBase):
+    def __init__(self, drive: DriveSubsystem):
+        self.drive_system = drive
+        self.addRequirements(drive)
+
+        # Maybe you don't need this: can't you schedule a command to run for n seconds?
+        self.timer = wpilib.Timer()
+
+    def initialize(self):
+        self.timer.start()
+        self.drive_system.io.setLed(True)
+
+    def execute(self):
+        self.drive_system.drive(-1.0, -0.5)
 
     def isFinished(self):
         return self.timer.get() > 2.0
